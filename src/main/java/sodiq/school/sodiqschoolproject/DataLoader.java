df233409332059197a26b66c6277b0822eaef69c
profix <davronsaydullayev2406@gmail.com>
2024-11-15 01:22:06 +0500

    Docker file changed

--- src/main/java/sodiq/school/sodiqschoolproject/DataLoader.java
+++ src/main/java/sodiq/school/sodiqschoolproject/DataLoader.java
@@ -29,21 +29,21 @@ public class DataLoader implements CommandLineRunner {
 
     @Override
     public void run(String... args) throws Exception {
-//        Permission[] permissions = Permission.values();
-//        Role admin = roleRepository.save(new Role("admin", Arrays.asList(permissions)));
-//        Role user = roleRepository.save(new Role("user", Arrays.asList(Permission.GET_MY_MEETINGS,Permission.BOOK_MEETING)));
-//        Role teacher = roleRepository.save(new Role("teacher", Arrays.asList(Permission.GET_MY_MEETINGS)));
-//        userRepository.save(new User("Admin", "admin","944288899", passwordEncoder.encode( "admin123"),admin,1));
-//        userRepository.save(new User("Davron", "Saydullayev","1111", passwordEncoder.encode( "1111"),roleRepository.findByName("teacher").get(),1));
-//        userRepository.save(new User("Rustam", "Saliboyev","2222", passwordEncoder.encode( "2222"),roleRepository.findByName("teacher").get(),1));
-//        userRepository.save(new User("Javohir", "O'rolov","3333", passwordEncoder.encode( "3333"),roleRepository.findByName("teacher").get(),1));
-//        userRepository.save(new User("DavronM", "Mavlonov","4444", passwordEncoder.encode( "4444"),roleRepository.findByName("teacher").get(),1));
-//        userRepository.save(new User("Asilbek", "Yallaboyev","5555", passwordEncoder.encode( "5555"),roleRepository.findByName("teacher").get(),1));
-//        userRepository.save(new User("user2", "user2","957301425", passwordEncoder.encode( "user2"),roleRepository.findByName("user").get(),1));
-//
-//        for (int i = 6; i < 100; i++) {
-//            userRepository.save(new User("user" + i, "user" + i, "" + i + i + i + i,  passwordEncoder.encode( "user" + i),roleRepository.findByName("user").get(),1));
-//        }
+        Permission[] permissions = Permission.values();
+        Role admin = roleRepository.save(new Role("admin", Arrays.asList(permissions)));
+        Role user = roleRepository.save(new Role("user", Arrays.asList(Permission.GET_MY_MEETINGS,Permission.BOOK_MEETING)));
+        Role teacher = roleRepository.save(new Role("teacher", Arrays.asList(Permission.GET_MY_MEETINGS)));
+        userRepository.save(new User("Admin", "admin","944288899", passwordEncoder.encode( "admin123"),admin,1));
+        userRepository.save(new User("Davron", "Saydullayev","1111", passwordEncoder.encode( "1111"),roleRepository.findByName("teacher").get(),1));
+        userRepository.save(new User("Rustam", "Saliboyev","2222", passwordEncoder.encode( "2222"),roleRepository.findByName("teacher").get(),1));
+        userRepository.save(new User("Javohir", "O'rolov","3333", passwordEncoder.encode( "3333"),roleRepository.findByName("teacher").get(),1));
+        userRepository.save(new User("DavronM", "Mavlonov","4444", passwordEncoder.encode( "4444"),roleRepository.findByName("teacher").get(),1));
+        userRepository.save(new User("Asilbek", "Yallaboyev","5555", passwordEncoder.encode( "5555"),roleRepository.findByName("teacher").get(),1));
+        userRepository.save(new User("user2", "user2","957301425", passwordEncoder.encode( "user2"),roleRepository.findByName("user").get(),1));
+
+        for (int i = 6; i < 100; i++) {
+            userRepository.save(new User("user" + i, "user" + i, "" + i + i + i + i,  passwordEncoder.encode( "user" + i),roleRepository.findByName("user").get(),1));
+        }
 
 //
 //        Sidebar sideBarMenu = new Sidebar("Questions","questions","quiz");
